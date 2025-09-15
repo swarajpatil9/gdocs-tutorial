af347511873cc63cdcdd0716af6eb96cf2626eb4
swarajpatil232 <swarajpatil2823@gmail.com>
2025-09-15 23:20:46 +0530

    remove unused authentication and document layout pages; update home page to use Link component

--- src/app/documents/setting/page.tsx
+++
@@ -1,9 +0,0 @@
-const DocumentsSettingsPage = () => {
-  return (
-    <div>
-        Document Setting Page
-    </div>
-  );
-};
-
-export default DocumentsSettingsPage;   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
-
+import Link from "next/link"; 
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>
-        Click Me
-      </Button>
+      Click <Link href="/documents/123">
+      <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
+      </Link> to go to document id page.
     </div>
   );
 };
