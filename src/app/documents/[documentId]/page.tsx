df2988935de13581672442d3f25181ed96328fad
swarajpatil232 <swarajpatil2823@gmail.com>
2025-09-16 00:01:42 +0530

    feat: integrate Tiptap editor and update document layout
    - Added Tiptap editor with starter kit to the document editor.
    - Updated package.json to include Tiptap dependencies and upgraded Next.js version.
    - Modified DocumentIdPage to include the new Editor component.
    - Created a new DocumentLayout component for consistent layout structure.

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 };
@@ -6,8 +8,8 @@ const DocumentIdPage = async ( { params }: DocumentIdPageProps) => {
     const { documentId } = await params;
 
   return ( 
-    <div>
-        Document ID : {documentId}
+    <div className="min-h-screen bg-[#FAFBFD]">
+        <Editor />
     </div>
   );
 };
